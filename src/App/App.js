3329ed5052bf89740020831612e59b164aca58ab
Tyler Stright <tylers@nezperce.org>
2023-02-16 10:32:01 -0700

    first success! managed to incorporate createBrowserRouter into build. Added Layout.

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import Layout from './Layout';
 import Home from '../Home/Home';
 import Footer from './Footer';
 import LoginForm from '../Forms/LoginForm';
@@ -7,6 +10,8 @@ import SignUpForm from '../Forms/SignUpForm';
 import NavBar from './NavBar';
 
 import axios from 'axios';
+
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
 import ListPage from '../ListPage/ListPage';
 import EditPage from '../AddEditPage/EditPage';
@@ -19,21 +24,19 @@ import ProjectView from '../Projects/ProjectView';
 import Facilities from '../Facilities/Facilities';
 import UserProfile from '../User/UserProfile';
 
+
 export default function App() {
   const [user, setUser] = useState(null);
 
-  // the common elements throughout the website are the NavBar and the background image.
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-
+  let router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
         <Route path="/accounts/login/" element={<LoginForm user={user} setUser={setUser} />} />
         <Route path="/accounts/signup/" element={<SignUpForm user={user} setUser={setUser} />} />
-
+  
         <Route path="/project/" element={<Projects />} />
-
+  
         {/* https://reactrouter.com/en/main/route/route */}
         <Route
           element={<ProjectView />}
@@ -54,27 +57,83 @@ export default function App() {
         // action
         // errorElement
         />
-
+  
         <Route path="/employee/list" element={<ListPage />} />
         <Route path="/project/list/" element={<ListPage />} />
         <Route path="/division/list" element={<ListPage />} />
         <Route path="/department/list" element={<ListPage />} />
-
+  
         {/* <Route path={['/employee/list/', '/project/list/','/division/list/','/department/list/']} element={<ListPage />} /> */}
-
+  
         <Route path="/employee/new/" element={<AddPage />} />
         <Route path="/department/new/" element={<AddPage />} />
         <Route path="/division/new/" element={<AddPage />} />
         <Route path="/project/new/" element={<AddPage />} />
-
+  
         <Route path="/project/:id/edit/" element={<EditPage />} />
-
-
-
+  
+  
+  
         <Route path="/facility" element={<Facilities />} />
         <Route path="/profile/" element={<UserProfile />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      </Route>
+    )
+  )
+
+
+  // the common elements throughout the website are the NavBar and the background image.
+  return ( <RouterProvider router={router} />
+    // <BrowserRouter>
+    //   <NavBar />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+
+    //     <Route path="/accounts/login/" element={<LoginForm user={user} setUser={setUser} />} />
+    //     <Route path="/accounts/signup/" element={<SignUpForm user={user} setUser={setUser} />} />
+
+    //     <Route path="/project/" element={<Projects />} />
+
+    //     {/* https://reactrouter.com/en/main/route/route */}
+    //     <Route
+    //       element={<ProjectView />}
+    //       path="/project/:id/"
+    //       loader={async ({ params }) => {
+    //         console.log('ProjectView Route.loader fired!');
+    //         return fetch(`/api/project/${params.id}.json`);
+    //         // axios.get(api) // USE THE PROXY!
+    //         //   .then(response => {
+    //         //     console.log('/api/project/:id/ response:');
+    //         //     console.log(response);
+    //         //     setProject(response.data);
+    //         //   })
+    //         //   .catch(error => {
+    //         //     console.log(error);
+    //         //   });
+    //       }}
+    //     // action
+    //     // errorElement
+    //     />
+
+    //     <Route path="/employee/list" element={<ListPage />} />
+    //     <Route path="/project/list/" element={<ListPage />} />
+    //     <Route path="/division/list" element={<ListPage />} />
+    //     <Route path="/department/list" element={<ListPage />} />
+
+    //     {/* <Route path={['/employee/list/', '/project/list/','/division/list/','/department/list/']} element={<ListPage />} /> */}
+
+    //     <Route path="/employee/new/" element={<AddPage />} />
+    //     <Route path="/department/new/" element={<AddPage />} />
+    //     <Route path="/division/new/" element={<AddPage />} />
+    //     <Route path="/project/new/" element={<AddPage />} />
+
+    //     <Route path="/project/:id/edit/" element={<EditPage />} />
+
+
+
+    //     <Route path="/facility" element={<Facilities />} />
+    //     <Route path="/profile/" element={<UserProfile />} />
+    //   </Routes>
+    //   <Footer />
+    // </BrowserRouter>
   );
 }
---
+++ src/App/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Footer from './Footer';
+import NavBar from './NavBar';
+
+
+export default function Layout() {
+
+    return (
+        <>
+            <NavBar />
+            <Outlet />
+            <Footer />
+        </>
+    );
+}
