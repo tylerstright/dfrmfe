1810e0e093d08fb6735b6112538cc659be5c13c8
Tyler Stright <tylers@nezperce.org>
2023-02-21 11:10:56 -0700

    improved employee and facility cards

--- src/Cards/FacilityCard.jsx
+++ src/Cards/FacilityCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col, Stack } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 export default function FacilityCard({ facility }) {
@@ -17,12 +17,15 @@ export default function FacilityCard({ facility }) {
                 onClick={redirect}
                 style={{ cursor: 'pointer' }}>
                 <Card.Body>
-                    <Card.Img variant="top" src={facility.facility_image} />
-                    <Card.Header as='h4' style={{ color: '#ABE7FC' }} >{facility.name.name}</Card.Header>
-                    <Card.Text>{facility.phone_number}</Card.Text>
-                    <Card.Text>{facility.street_address}</Card.Text>
-                    <Card.Text>{facility.mailing_address}</Card.Text>
+                    <Card.Img className='overflow-hidden' src={facility.facility_image} style={{ minWidth: '300px', maxHeight: '300px' }} />
+
+                    <Card.Header as='h4' className ='mt-3' style={{ color: '#ABE7FC' }} >{facility.name.name}</Card.Header>
                     <Card.Text className={'text-muted'}>Manager: {`${facility.manager.name.user.first_name} ${facility.manager.name.user.last_name}`}</Card.Text>
+                    <Stack className='my-3' gap={0}>
+                        <Card.Text>{facility.phone_number} </Card.Text>
+                        <Card.Text>{facility.street_address}</Card.Text>
+                        <Card.Text >{facility.mailing_address}</Card.Text>
+                    </Stack>
                 </Card.Body>
             </Card>
         </Col>
